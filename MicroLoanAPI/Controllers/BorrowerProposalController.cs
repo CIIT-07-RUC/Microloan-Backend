572e8238fed9186c03c1c3b13882570ce13ab0ba
Matúš Kalanin <mato.kalanin@gmail.com>
2024-05-19 14:03:22 +0200

    separate borrower controller from loan controller

---
+++ MicroLoanAPI/Controllers/BorrowerProposalController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using DataService;
+using Idfy;
+using Idfy.IdentificationV2;
+using MicroLoanAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroLoanAPI.Controllers
+{
+    [Route("api/loan")]
+    [ApiController]
+    public class BorrowerProposal : ControllerBase
+	{
+        private readonly IDataService _dataservice;
+
+        public BorrowerProposal(IDataService dataService)
+		{
+            
+            _dataservice = dataService;
+        }
+
+        [HttpPost("borrower-proposal")]
+        public IActionResult CreateBorrowerProposal(BorrowerProposalModel model)
+        {
+
+    
+            var borrowerProposal = _dataservice.CreateBorrowerProposal(model.BorrowerId, model.ProposalInterestRate, model.ProposalAmount, model.ProposalMonths);
+
+            bool isActionSuccess = borrowerProposal.Item1;
+            string responseMessage = borrowerProposal.Item2;
+
+            if (isActionSuccess == false)
+            {
+                return BadRequest(new { isRegistrationSuccessful = isActionSuccess, responseMessage = responseMessage });
+            }
+
+            return Ok(new { isRegistrationSuccessful = isActionSuccess, responseMessage = responseMessage });
+        }
+
+       
+
+        [HttpGet("borrower-proposals")]
+        public IActionResult GetBorrowerProposals()
+        {
+            var borrowerProposals = _dataservice.GetBorrowerProposals();
+            return Ok(borrowerProposals);
+        }
+
+    }
+}
+
--- MicroLoanAPI/Controllers/LoanController.cs
+++ MicroLoanAPI/Controllers/LoanController.cs
@@ -19,33 +19,6 @@ namespace MicroLoanAPI.Controllers
             _dataservice = dataService;
         }
 
-        [HttpPost("borrower-proposal")]
-        public IActionResult CreateBorrowerProposal(BorrowerProposalModel model)
-        {
-
-    
-            var borrowerProposal = _dataservice.CreateBorrowerProposal(model.BorrowerId, model.ProposalInterestRate, model.ProposalAmount, model.ProposalMonths);
-
-            bool isActionSuccess = borrowerProposal.Item1;
-            string responseMessage = borrowerProposal.Item2;
-
-            if (isActionSuccess == false)
-            {
-                return BadRequest(new { isRegistrationSuccessful = isActionSuccess, responseMessage = responseMessage });
-            }
-
-            return Ok(new { isRegistrationSuccessful = isActionSuccess, responseMessage = responseMessage });
-        }
-
-       
-
-        [HttpGet("borrower-proposals")]
-        public IActionResult GetBorrowerProposals()
-        {
-            var borrowerProposals = _dataservice.GetBorrowerProposals();
-            return Ok(borrowerProposals);
-        }
-
     }
 }
 
